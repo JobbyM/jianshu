9ad8010c2c43d7a86616d505907497b87f01c3c0
JobbyM <songzhipengmune@gmail.com>
2018-09-30 16:28:14 +0800

    异步获取详情页信息

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { actionCreators } from './store'
 import {
   DetailWrapper,
   Header,
@@ -7,19 +9,28 @@ import {
 
 class Detail extends Component {
   render() {
+    const { title ,content } = this.props
     return (
       <DetailWrapper>
-        <Header>她是黄晓明的前女友，却在演了富察皇后秒杀杨颖</Header>
-        <Content>
-          <img alt='' src='https://upload-images.jianshu.io/upload_images/13132640-834754b7ec8b3f7d.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/625/format/webp' />
-          <p>剧中的富察皇后，是一位温柔贤德的好皇后，宽容大度，生气起来都表现出无比温婉的模样，与我们以前在宫廷戏里见过的皇后有着极大的区别。而由秦岚来饰演这一角色，简直是最合适不过的了，不仅颜值在线，美得像画里走出来的人儿，而且演技在线，连无比温柔的声音都还容易让人产生好感</p>
-          <p>剧中的富察皇后，是一位温柔贤德的好皇后，宽容大度，生气起来都表现出无比温婉的模样，与我们以前在宫廷戏里见过的皇后有着极大的区别。而由秦岚来饰演这一角色，简直是最合适不过的了，不仅颜值在线，美得像画里走出来的人儿，而且演技在线，连无比温柔的声音都还容易让人产生好感</p>
-          <p>剧中的富察皇后，是一位温柔贤德的好皇后，宽容大度，生气起来都表现出无比温婉的模样，与我们以前在宫廷戏里见过的皇后有着极大的区别。而由秦岚来饰演这一角色，简直是最合适不过的了，不仅颜值在线，美得像画里走出来的人儿，而且演技在线，连无比温柔的声音都还容易让人产生好感</p>
-          <p>剧中的富察皇后，是一位温柔贤德的好皇后，宽容大度，生气起来都表现出无比温婉的模样，与我们以前在宫廷戏里见过的皇后有着极大的区别。而由秦岚来饰演这一角色，简直是最合适不过的了，不仅颜值在线，美得像画里走出来的人儿，而且演技在线，连无比温柔的声音都还容易让人产生好感</p>
-        </Content>
+        <Header>{title}</Header>
+        <Content dangerouslySetInnerHTML={{__html: content}} />
       </DetailWrapper>
     )
   }
+
+  componentDidMount() {
+    this.props.getDetail()
+  }
 }
 
-export default Detail
+const mapState = (state) => ({
+  title: state.getIn(['detail', 'title']),
+  content: state.getIn(['detail', 'content'])
+})
+
+const mapDispatch = (dispatch) => ({
+  getDetail() {
+    dispatch(actionCreators.getDetail())
+  }
+})
+export default connect(mapState, mapDispatch)(Detail)
